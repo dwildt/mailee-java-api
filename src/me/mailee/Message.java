63b95a3b1fc6c3c00a97982ded7271e382ec0cb6
Diogo Biazus <diogob@gmail.com>
2010-12-01 11:54:16 -0200

    Put all members in Message.

--- src/me/mailee/Message.java
+++ src/me/mailee/Message.java
@@ -1,6 +1,7 @@
 package me.mailee;
 
 import org.jactiveresource.ActiveResource;
+import java.util.Date;
 
 public class Message extends ActiveResource {
 	private String id;
@@ -9,6 +10,30 @@ public class Message extends ActiveResource {
 	private String fromEmail;
 	private String fromName;
 	private Integer listId;
+	private String analytics;
+	private String checked;
+	private Integer clientId;
+	private String contentFrom;
+	private Date createdAt;
+	private Date deletedAt;
+	private String html;
+	private String htmlFromUrl;
+	private String log;
+	private Integer newsletterId;
+	private String replyEmail;
+	private String rss;
+	private Integer segmentId;
+	private Date sendAfter;
+	private Date sendWhen;
+	private Integer status;
+	private Integer templateId;
+	private String text;
+	private String thumbContentType;
+	private String thumbFileName;
+	private Integer thumbFileSize;
+	private Date thumbUpdatedAt;
+	private Integer total;
+	private String url;
 
 	public void setId(String id) {
 		this.id = id;
@@ -57,5 +82,197 @@ public class Message extends ActiveResource {
 
 	public void setFromName(String fromName) {
 		this.fromName = fromName;
+	}
+
+	public String getAnalytics() {
+		return analytics;
+	}
+
+	public void setAnalytics(String analytics) {
+		this.analytics = analytics;
+	}
+
+	public String getChecked() {
+		return checked;
+	}
+
+	public void setChecked(String checked) {
+		this.checked = checked;
+	}
+
+	public Integer getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Integer clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getContentFrom() {
+		return contentFrom;
+	}
+
+	public void setContentFrom(String contentFrom) {
+		this.contentFrom = contentFrom;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public String getHtml() {
+		return html;
+	}
+
+	public void setHtml(String html) {
+		this.html = html;
+	}
+
+	public String getHtmlFromUrl() {
+		return htmlFromUrl;
+	}
+
+	public void setHtmlFromUrl(String htmlFromUrl) {
+		this.htmlFromUrl = htmlFromUrl;
+	}
+
+	public String getLog() {
+		return log;
+	}
+
+	public void setLog(String log) {
+		this.log = log;
+	}
+
+	public Integer getNewsletterId() {
+		return newsletterId;
+	}
+
+	public void setNewsletterId(Integer newsletterId) {
+		this.newsletterId = newsletterId;
+	}
+
+	public String getReplyEmail() {
+		return replyEmail;
+	}
+
+	public void setReplyEmail(String replyEmail) {
+		this.replyEmail = replyEmail;
+	}
+
+	public String getRss() {
+		return rss;
+	}
+
+	public void setRss(String rss) {
+		this.rss = rss;
+	}
+
+	public Integer getSegmentId() {
+		return segmentId;
+	}
+
+	public void setSegmentId(Integer segmentId) {
+		this.segmentId = segmentId;
+	}
+
+	public Date getSendAfter() {
+		return sendAfter;
+	}
+
+	public void setSendAfter(Date sendAfter) {
+		this.sendAfter = sendAfter;
+	}
+
+	public Date getSendWhen() {
+		return sendWhen;
+	}
+
+	public void setSendWhen(Date sendWhen) {
+		this.sendWhen = sendWhen;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getTemplateId() {
+		return templateId;
+	}
+
+	public void setTemplateId(Integer templateId) {
+		this.templateId = templateId;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getThumbContentType() {
+		return thumbContentType;
+	}
+
+	public void setThumbContentType(String thumbContentType) {
+		this.thumbContentType = thumbContentType;
+	}
+
+	public String getThumbFileName() {
+		return thumbFileName;
+	}
+
+	public void setThumbFileName(String thumbFileName) {
+		this.thumbFileName = thumbFileName;
+	}
+
+	public Integer getThumbFileSize() {
+		return thumbFileSize;
+	}
+
+	public void setThumbFileSize(Integer thumbFileSize) {
+		this.thumbFileSize = thumbFileSize;
+	}
+
+	public Date getThumbUpdatedAt() {
+		return thumbUpdatedAt;
+	}
+
+	public void setThumbUpdatedAt(Date thumbUpdatedAt) {
+		this.thumbUpdatedAt = thumbUpdatedAt;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 }
